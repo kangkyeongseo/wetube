9c2a971f0046416df6db2dc277652d411d507c33
kangkyeongseo <kks_big@naver.com>
2022-04-11 15:10:33 +0900

    Set Up Express

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import morgan from "morgan";
+
+const PORT = 4000;
+
+const app = express();
+const logger = morgan("dev");
+
+const handleHome = (req, res) => {
+  return res.end();
+};
+
+app.use(logger);
+app.get("/", handleHome);
+
+const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}🚀`);
+
+app.listen(PORT, handleListening);

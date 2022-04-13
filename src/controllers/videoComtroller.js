49e91c4a3a89f15fbfd442053718e2b118f34922
kangkyeongseo <kks_big@naver.com>
2022-04-13 16:23:20 +0900

    Set UP PUG

--- src/controllers/videoComtroller.js
+++ src/controllers/videoComtroller.js
@@ -1,8 +1,34 @@
-export const trending = (req, res) => res.send("Home Page Videos");
-export const see = (req, res) => {
-    return res.send(`Watch Video ${req.params.id}`);
-}
-export const edit = (req, res) => res.send("Edit Video");
+export const trending = (req, res) => {
+    const videos = [
+        {
+            title: "First Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            view: 59,
+            id:1,
+        },
+        {
+            title: "Second Video",
+            rating: 4,
+            comments: 3,
+            createdAt: "1 minutes ago",
+            view: 39,
+            id:2,
+        },
+        {
+            title: "Third Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            view: 59,
+            id:1,
+        },
+    ];
+    return res.render("home", {pageTitle: "Home", videos});
+};
+export const see = (req, res) => res.render("watch", {pageTitle: "Watch"});
+export const edit = (req, res) => res.render("edit", {pageTitle: "Edit"});
 export const search = (req, res) => res.send("Search Video");
 export const deleteVideo = (req, res) => res.send("Delete Video");
 export const upload = (req, res) => res.send("Upload Video");

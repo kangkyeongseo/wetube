9b36ddd994f89af64e408dbde136395d9ebed58d
kangkyeongseo <kks_big@naver.com>
2022-04-15 23:05:40 +0900

    Build Video Model(Upload, Find)

--- src/controllers/videoComtroller.js
+++ src/controllers/videoComtroller.js
@@ -1,63 +1,39 @@
-let videos = [
-    {
-        title: "First Video",
-        rating: 5,
-        comments: 2,
-        createdAt: "2 minutes ago",
-        view: 59,
-        id:1,
-    },
-    {
-        title: "Second Video",
-        rating: 4,
-        comments: 3,
-        createdAt: "1 minutes ago",
-        view: 1,
-        id:2,
-    },
-    {
-        title: "Third Video",
-        rating: 5,
-        comments: 2,
-        createdAt: "2 minutes ago",
-        view: 59,
-        id:3,
-    },
-];
+import Video from "../models/Video"
 
-export const trending = (req, res) => {
+export const home = async(req, res) => {
+    const videos = await Video.find({})
     return res.render("home", {pageTitle: "Home", videos});
 };
-export const watch = (req, res) => {
+export const watch = async(req, res) => {
     const { id } = req.params;
-    const video = videos[id-1];
-    return res.render("watch", {pageTitle: `Watching ${video.title}`, video})
+    const video = await Video.findById(id);
+    return res.render("watch", {pageTitle: video.title, video})
 };
 export const getEdit = (req, res) => {
     const { id } = req.params;
-    const video = videos[id-1];
-    return res.render("edit", {pageTitle: `Editing ${video.title}`, video});
+    return res.render("edit", {pageTitle: `Editing video`});
 };
 export const postEdit = (req, res) => {
     const { id } = req.params;
     const { title } = req.body;
-    videos[id-1].title = title;
     return res.redirect(`/videos/${id}`);
 };
 export const getUpload = (req, res) => {
     return res.render("upload", {pageTitle:"Upload Video"});
 };
-export const postUpload = (req, res) => {
-    const {title} = req.body
-    const newVideo = {
-        title,
-        rating: 0,
-        comments: 0,
-        createdAt: "just now",
-        view: 0,
-        id:videos.length + 1,
-    }
-    videos.push(newVideo);
+export const postUpload = async(req, res) => {
+    const {title, description, hashtags} = req.body;
+    try {
+        await Video.create({
+            title,
+            description,
+            hashtags: hashtags.split(",").map(word => `#${word}`),
+        });
+    }catch(error) {
+        console.log(error);
+        return res.render("upload", {pageTitle:"Upload Video", errorMessage: error._message});
+    };
+    
     return res.redirect("/");
 };
 

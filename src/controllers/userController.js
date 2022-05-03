8f97d6edde74bbcdfd45ecfb57bfc24d6e994f37
kangkyeongseo <kks_big@naver.com>
2022-05-03 16:00:19 +0900

    isHeroku

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -163,10 +163,11 @@ export const postEdit = async (req, res) => {
         errorMessage: "This username is already in use.",
       });
     } else {
+      const isHeroku = process.env.NODE_ENV === "production";
       const updatedUser = await User.findByIdAndUpdate(
         _id,
         {
-          avatarUrl: file ? file.location : avatarUrl,
+          avatarUrl: file ? (isHeroku ? file.location : file.path) : avatarUrl,
           name,
           email,
           username,
